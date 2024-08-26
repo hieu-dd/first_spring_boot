e75a351c4b4062784e451ca1ee3ba5a438a19705
14H <do.duc.hieu@linecorp.com>
2024-08-26 16:18:25 +0700

    Security with jdbc

--- src/main/kotlin/com/bakarot/demoapicurd/security/StudentSecurityConfigure.kt
+++ src/main/kotlin/com/bakarot/demoapicurd/security/StudentSecurityConfigure.kt
@@ -5,31 +5,41 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.provisioning.JdbcUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
+import javax.sql.DataSource
 
 @Configuration
 class StudentSecurityConfigure {
 
+//    @Bean
+//    fun userDetailsManagers(): InMemoryUserDetailsManager {
+//        val employee = User.builder()
+//            .username("employee")
+//            .password("{noop}employee")
+//            .roles("EMPLOYEE")
+//            .build()
+//        val manager = User.builder()
+//            .username("manager")
+//            .password("{noop}manager")
+//            .roles("MANAGER")
+//            .build()
+//        val admin = User.builder()
+//            .username("admin")
+//            .password("{noop}admin")
+//            .roles("ADMIN")
+//            .build()
+//        return InMemoryUserDetailsManager(employee, manager, admin)
+//    }
+
+    // use jdbc to avoid hardcode user
     @Bean
-    fun userDetailsManagers(): InMemoryUserDetailsManager {
-        val employee = User.builder()
-            .username("employee")
-            .password("{noop}employee")
-            .roles("EMPLOYEE")
-            .build()
-        val manager = User.builder()
-            .username("manager")
-            .password("{noop}manager")
-            .roles("MANAGER")
-            .build()
-        val admin = User.builder()
-            .username("admin")
-            .password("{noop}admin")
-            .roles("ADMIN")
-            .build()
-        return InMemoryUserDetailsManager(employee, manager, admin)
+    fun userDetailsManagers(dataSource: DataSource): JdbcUserDetailsManager {
+        return JdbcUserDetailsManager(dataSource).apply {
+//            enable when need custom table authority
+//            usersByUsernameQuery = "select username, password, enabled from users where username = ?"
+//            setAuthoritiesByUsernameQuery("select username, authority from authorities where username = ?")
+        }
     }
 
     @Bean
